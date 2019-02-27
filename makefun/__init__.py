ce893cb5f36ce164d94409ad12cd8315795414e5
Sylvain MARIE <sylvain.marie@se.com>
2019-02-27 14:38:00 +0100

    reverted previous commit - this was a bad idea

--- makefun/__init__.py
+++ makefun/__init__.py
@@ -1,9 +1,8 @@
-from makefun.main import create_function, with_signature, remove_signature_parameters, add_signature_parameters, \
-    signature
+from makefun.main import create_function, with_signature, remove_signature_parameters, add_signature_parameters
 
 __all__ = [
     # submodules
     'main',
     # symbols
-    'create_function', 'with_signature', 'remove_signature_parameters', 'add_signature_parameters', 'signature'
+    'create_function', 'with_signature', 'remove_signature_parameters', 'add_signature_parameters'
 ]
