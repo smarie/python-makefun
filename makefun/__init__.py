a94c1c6dd29aa6114c66bcb0216ad00a6dde3b0d
Sylvain MARIE <sylvain.marie@se.com>
2019-02-27 14:30:01 +0100

    A new `signature` alias is provided, so that users do not have to handle the `inspect` / `funcsigs` management complexity. Simply import the alias from `makefun` !

--- makefun/__init__.py
+++ makefun/__init__.py
@@ -1,8 +1,9 @@
-from makefun.main import create_function, with_signature, remove_signature_parameters, add_signature_parameters
+from makefun.main import create_function, with_signature, remove_signature_parameters, add_signature_parameters, \
+    signature
 
 __all__ = [
     # submodules
     'main',
     # symbols
-    'create_function', 'with_signature', 'remove_signature_parameters', 'add_signature_parameters'
+    'create_function', 'with_signature', 'remove_signature_parameters', 'add_signature_parameters', 'signature'
 ]
